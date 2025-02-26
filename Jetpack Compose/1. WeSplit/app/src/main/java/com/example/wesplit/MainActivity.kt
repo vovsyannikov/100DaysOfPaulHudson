7737e2a98e3ec879f397884b76f7e8b79a05af66
Виталий Овсянников <its.vint@ya.ru>
2025-02-26 18:45:59 +0300

    Replaced deprecated method with actual

--- Jetpack Compose/1. WeSplit/app/src/main/java/com/example/wesplit/MainActivity.kt
+++ Jetpack Compose/1. WeSplit/app/src/main/java/com/example/wesplit/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -120,7 +121,7 @@ fun ContentView(modifier: Modifier = Modifier) {
                 label = { Text("Number of people") },
                 onValueChange = {},
                 modifier = Modifier
-                    .menuAnchor()
+                    .menuAnchor(MenuAnchorType.PrimaryEditable)
                     .fillMaxWidth()
             )
 
